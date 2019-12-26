b991569a606158c319ab3807afabb3e9396b06f7
damienk <damienk@berkeley.edu>
2019-12-25 17:04:01 -0800

    gui

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import pygame
 import solver
+import copy
 
 pygame.init()
 win = pygame.display.set_mode((495, 495))
@@ -19,67 +20,94 @@ win.fill(blue)
 def run(board):
     init_board()
     set_board(board)
-    orig_board = board.copy()
+    orig_board = copy.deepcopy(board)
 
     selected = False
     valued = False
+    prev_val = -1
     while True:
         for event in pygame.event.get():
+
+            # Game exit.
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+
+            # Key pressed.
             elif event.type == pygame.KEYDOWN:
+
+                # Pressed space bar which solves the sudoku game using backtracking.
                 if event.key == pygame.K_SPACE:
+                    if selected:
+                        reset_selected_box(valued, prev_pos, prev_val)
+                        selected = False
+                        valued = False
                     solve_gui(board)
 
                 # Pressed number character with box selected.
                 elif event.key in [pygame.K_1, pygame.K_2, pygame.K_3,
                                    pygame.K_4, pygame.K_5, pygame.K_6,
                                    pygame.K_7, pygame.K_8, pygame.K_9]:
-                    # if selected:
-                    #     col, row = prev_pos[0] // 55, prev_pos[1] // 55
-                    #     if (orig_board[row][col] == 0) & ():
-                    #         board[row][col] = 0
-                    #         delay_draw(col, row, board[row][col], blue, 0)
-
                     if selected:
-                        if valued:
-                            delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, prev_val, blue, 0)
-                        else:
-                            delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, "?", blue, 0)
-                        delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, event.unicode, grey, 0)
+                        reset_selected_box(valued, prev_pos, prev_val)
                         valued = True
+                        delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, event.unicode, grey, 0)
                         prev_val = event.unicode
 
                 # Pressing enter with a box filled in with value.
                 # Value is either valid or invalid.
                 elif event.key == pygame.K_RETURN:
+
+                    # Enter only works if there is a box selected and valued.
                     if selected & valued:
                         value = int(prev_val)
                         col, row = prev_pos[0] // 55, prev_pos[1] // 55
+
+                        # Input is valid.
                         if solver.is_valid(row, col, value, board):
                             board[row][col] = value
                             delay_draw(col, row, value, green, 200)
                             delay_draw(col, row, value, black, 0)
+
+                        # Input is invalid.
                         else:
                             delay_draw(col, row, value, red, 200)
                             delay_draw(col, row, value, blue, 0)
                         selected = False
                         valued = False
+                        prev_val = -1
 
             # Mouse selection.
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
-                if empty_pos(x, y, board):
+                col, row = x // 55, y // 55
+
+                # Selected box can be changed.
+                if empty_pos(x, y, orig_board):
+                    if board[row][col] != 0:
+                        delay_draw(col, row, board[row][col], blue, 0)
+                        board[row][col] = 0
                     if selected:
-                        if valued:
-                            delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, prev_val, blue, 0)
-                            valued = False
-                        else:
-                            delay_draw(prev_pos[0] // 55, prev_pos[1] // 55, "?", blue, 0)
-                    delay_draw(x // 55, y // 55, "?", grey, 0)
+                        reset_selected_box(valued, prev_pos, prev_val)
+                        valued = False
+                    delay_draw(col, row, "?", grey, 0)
                     selected = True
                     prev_pos = (x, y)
+
+
+def reset_selected_box(valued, prev_pos, prev_val):
+    """
+    Resets previously selected box to 0 graphically.
+    :param valued: boolean for whether the selected box has been filled.
+    :param prev_pos: position of previously selected box.
+    :param prev_val: value of previously selected box. null value represented by -1.
+    :return: none.
+    """
+    prev_col, prev_row = prev_pos[0] // 55, prev_pos[1] // 55
+    if valued & (prev_val != -1):
+        delay_draw(prev_col, prev_row, prev_val, blue, 0)
+    else:
+        delay_draw(prev_col, prev_row, "?", blue, 0)
 
 
 def empty_pos(x, y, board):
@@ -111,11 +139,11 @@ def solve_gui(board):
     for i in range(1, 10):
         if solver.is_valid(row, col, i, board):
             board[row][col] = i
-            delay_draw(col, row, i, white, 80)
+            delay_draw(col, row, i, black, 90)
             if solve_gui(board):
                 return True
             board[row][col] = 0
-            delay_draw(col, row, i, blue, 80)
+            delay_draw(col, row, i, blue, 90)
     return False
 
 
